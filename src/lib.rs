171936b8f52860c09a9712ee5465ef38062d2c9b
Mason Fischer <mason@kissr.co>
2019-07-08 14:53:19 -0400

    Bump to version "0.1.2"

--- src/lib.rs
+++ src/lib.rs
@@ -1,112 +1,85 @@
-#![no_std]
-#![feature(alloc)]
-#[macro_use]
-extern crate std;
-extern crate alloc;
-extern crate wasm_rpc;
 extern crate ellipticoin;
-extern crate secp256k1;
+extern crate wasm_rpc;
+extern crate wasm_rpc_macros;
 
-use alloc::vec::Vec;
-use alloc::string::String;
-use alloc::collections::btree_map::BTreeMap;
-use self::secp256k1::{Secp256k1};
-use wasm_rpc::*;
-use wasm_rpc::{Bytes, Dereferenceable, Referenceable};
 use std::cell::RefCell;
-use core::intrinsics::transmute;
+use std::collections::BTreeMap;
+pub use wasm_rpc::{Bytes, FromBytes, ToBytes, Value};
+pub use wasm_rpc_macros::export;
 
 thread_local!(static SENDER: RefCell<Vec<u8>> = RefCell::new(Vec::new()));
 thread_local!(static BLOCK_WINNER: RefCell<Vec<u8>> = RefCell::new(Vec::new()));
-thread_local!(static STATE: RefCell<BTreeMap<Vec<u8>, Vec<u8>>> = RefCell::new(BTreeMap::new()));
+thread_local!(static BLOCK_NUMBER: RefCell<u64> = RefCell::new(0));
+thread_local!(static MEMORY: RefCell<BTreeMap<Vec<u8>, Vec<u8>>> = RefCell::new(BTreeMap::new()));
+thread_local!(static STORAGE: RefCell<BTreeMap<Vec<u8>, Vec<u8>>> = RefCell::new(BTreeMap::new()));
 
+pub mod error;
 
 pub fn set_sender(sender: Vec<u8>) {
-    SENDER.with(|sender_cell|{
-        sender_cell.replace(sender)
-    });
+    SENDER.with(|sender_cell| sender_cell.replace(sender));
 }
 
 pub fn set_block_winner(block_winner: Vec<u8>) {
-    BLOCK_WINNER.with(|block_winner_cell|{
-        block_winner_cell.replace(block_winner)
-    });
+    BLOCK_WINNER.with(|block_winner_cell| block_winner_cell.replace(block_winner));
 }
 
-pub fn read<K: Into<Vec<u8>>>(key: K) -> Vec<u8> {
-    STATE.with(|state_cell|{
+pub fn set_block_number(block_number: u64) {
+    BLOCK_NUMBER.with(|block_number_cell| block_number_cell.replace(block_number));
+}
+
+pub fn get_memory<K: ToBytes, V: FromBytes>(key: K) -> V {
+    let v: Vec<u8> = MEMORY.with(|state_cell| {
         let state = state_cell.borrow_mut();
-        match state.get(&key.into()) {
+        match state.get::<Vec<u8>>(&key.to_bytes()) {
             Some(value) => value.to_vec(),
             None => vec![],
         }
+    });
+    FromBytes::from_bytes(v)
+}
+
+pub fn set_memory<K: ToBytes, V: ToBytes>(key: K, value: V) {
+    MEMORY.with(|state_cell| {
+        let mut state = state_cell.borrow_mut();
+        state.insert(key.to_bytes(), value.to_bytes());
     })
 }
 
-pub fn write<K: Into<Vec<u8>>>(key: K, value: Vec<u8>) {
-    STATE.with(|state_cell|{
-        let mut state = state_cell.borrow_mut();
-        state.insert(key.into(), value);
-    })
-}
-
-pub fn update<K: Into<Vec<u8>>>(key: K, update_function: &Fn(Vec<u8>) -> Vec<u8>) {
-    STATE.with(|state_cell|{
-        let mut state = state_cell.borrow_mut();
-        let key_vec = key.into();
-        let value_before = match state.get(&key_vec.clone()) {
+pub fn get_storage<K: ToBytes, V: FromBytes>(key: K) -> V {
+    let v: Vec<u8> = STORAGE.with(|state_cell| {
+        let state = state_cell.borrow_mut();
+        match state.get::<Vec<u8>>(&key.to_bytes()) {
             Some(value) => value.to_vec(),
             None => vec![],
-        };
-        let value_after = update_function(value_before.into());
-        state.insert(key_vec, value_after);
+        }
+    });
+    FromBytes::from_bytes(v)
+}
+
+pub fn set_storage<K: ToBytes, V: ToBytes>(key: K, value: V) {
+    STORAGE.with(|state_cell| {
+        let mut state = state_cell.borrow_mut();
+        state.insert(key.to_bytes(), value.to_bytes());
     })
 }
 
-pub fn sender() -> Vec<u8>{
-    SENDER.with(|sender_cell|{
-        sender_cell.borrow().to_vec()
-    })
+pub fn sender() -> Vec<u8> {
+    SENDER.with(|sender_cell| sender_cell.borrow().to_vec())
 }
 
-pub fn block_winner() -> Vec<u8>{
-    BLOCK_WINNER.with(|block_winner_cell|{
-        block_winner_cell.borrow().to_vec()
-    })
+pub fn block_winner() -> Vec<u8> {
+    BLOCK_WINNER.with(|block_winner_cell| block_winner_cell.borrow().to_vec())
 }
 
-pub fn read_u32<K: Into<Vec<u8>>>(key: K) -> u32 {
-    read(key.into()).value()
+pub fn block_number() -> u64 {
+    BLOCK_NUMBER.with(|block_number_cell| *block_number_cell.borrow())
 }
 
-pub fn read_u64<K: Into<Vec<u8>>>(key: K) -> u64 {
-    read(key.into()).value()
-}
-
-pub fn write_u32<K: Into<Vec<u8>>>(key: K, value: u32) {
-    write(key.into(), unsafe{transmute::<u32, [u8; 4]>(value).to_vec()});
-}
-
-pub fn write_u64<K: Into<Vec<u8>>>(key: K, value: u64) {
-    write(key.into(), unsafe{transmute::<u64, [u8; 8]>(value).to_vec()});
-}
-
-pub fn write_int<K: Into<Vec<u8>>>(key: K, value: u64) {
-    write(key.into(), unsafe{transmute::<u64, [u8; 8]>(value).to_vec()});
-}
-
-pub fn read_int<K: Into<Vec<u8>>>(key: K) -> u64 {
-    read_u64(key.into())
-}
-
-pub fn secp256k1_recover(message_vec: Vec<u8>, signature_vec: Vec<u8>, recovery_id: u8) -> Vec<u8> {
-    let s = Secp256k1::new();
-    let message = secp256k1::Message::from_slice(&message_vec).unwrap();
-    let signature = secp256k1::RecoverableSignature::from_compact(&s, &signature_vec, secp256k1::RecoveryId::from_i32(recovery_id as i32).unwrap()).unwrap();
-    println!("{:?}", message);
-    s.recover(&message, &signature).unwrap().serialize().to_vec()
-}
-
-pub fn call(_code: Vec<u8>, _method: String, _params: Vec<u8>, _storage_context: Vec<u8>) -> Vec<u8> {
+pub fn call(
+    _code: Vec<u8>,
+    _method: String,
+    _params: Vec<u8>,
+    _storage_context: Vec<u8>,
+) -> Vec<u8> {
     unreachable!();
 }
